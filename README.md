1b1f080b2988dfb217977031934498a063f8f781
Asim Ihsan <asim.ihsan@gmail.com>
2013-06-09 12:47:19 +0100

    Trying to add namespacing to the module...

--- README.md
+++ README.md
@@ -30,6 +30,16 @@ $ jison grammar.jison; echo "index = 5 or not ('first name' = dude)" > testgramm
 [ 'OR',
   [ 'EQ', 'index', '5' ],
   [ 'NOT', [ 'EQ', 'first name', 'dude' ] ] ]
+
+$ jison grammar.jison; closure-compiler --js grammar.js --js_output_file grammar.min.js; echo "index = 5 && !('first name' == dude)" > testgrammar; node grammar.min.js testgrammar
+grammar.js:238: WARNING - unreachable code
+    return true;
+    ^
+
+0 error(s), 1 warning(s)
+[ 'AND',
+  [ 'EQ', 'index', '5' ],
+  [ 'NOT', [ 'EQ', 'first name', 'dude' ] ] ]
 ```
 
 -   Open the HTML test page in a web browser and confirm the following filters work:
--- src/tablequery.js
+++ src/tablequery.js
@@ -11,16 +11,14 @@ not ('first name' ilike mark or number = 2)
 
 */
 
-
 $(document).ready(function() {
-    var parsed_query;
+    var table; // = $("table");
+    var table_search_text; // = $("#table_search_text");
+    var table_search_text_control_group; // = table_search_text.closest(".control-group");
+
     var parser_exception_message;
     var previous_search_text;
     var table_headings = {};
-    $.map($("#table th"), function(el, i) {
-        table_headings[el.textContent.toLowerCase()] = i; 
-    });
-    console.log(table_headings);
 
     get_rows_to_display = function(trees) {
         console.log("get_rows_to_display.");
@@ -51,7 +49,7 @@ $(document).ready(function() {
             if (left in table_headings) {
                 console.log("left value in table_headings.");
                 var column_index = table_headings[left];
-                return_value = $("table tbody tr").filter(function(i) {
+                return_value = table.find("tbody tr").filter(function(i) {
                     text = $(this).children()[column_index].textContent;
                     regexp_result = expr.test(text);
                     if (test_for_true) {
@@ -80,7 +78,7 @@ $(document).ready(function() {
                 // I do I can't get elements to match under ===. Why?
                 // return_value = _.difference($("table tbody tr"), left_trees);
 
-                return_value = _.filter($("table tbody tr"), function(el) {
+                return_value = _.filter(table.find("tbody tr"), function(el) {
                     var el_in_left_trees = false;
                     _.each(left_trees, function(el2) {
                         if ($(el).index() == $(el2).index()) {
@@ -93,7 +91,7 @@ $(document).ready(function() {
         }
         if (!return_value) {
             console.log("no return value, return all rows.");
-            return_value = $("table tbody tr");
+            return_value = table.find("tbody tr");
         }
         console.log("returning")
         console.log(return_value);
@@ -101,50 +99,67 @@ $(document).ready(function() {
     }
 
     parse_search_text = function(search_text) {
-        is_parse_successful = true;
+        var is_parse_successful = true;
+        var parsed_query;
         try {
             parsed_query = grammar.parse(search_text);
         } catch(e) {
             parser_exception_message = e.message;
             is_parse_successful = false;
         }
-        return is_parse_successful;        
+        return {"rc": is_parse_successful, "parsed_query": parsed_query};        
     }
 
-    update_table_search_text_warning = function() {
-        if ($("#table_search_text").val().length == 0) {
-            $("#table_search_text_control_group").removeClass("error");
-            $("#table_search_text_control_group").removeClass("success");
+    update_table_search_text_warning = function(is_parse_successful) {
+        if (table_search_text.val().length == 0) {
+            table_search_text_control_group.removeClass("error");
+            table_search_text_control_group.removeClass("success");
         } else if (is_parse_successful) {
-            $("#table_search_text_control_group").removeClass("error");
-            $("#table_search_text_control_group").addClass("success");
+            table_search_text_control_group.removeClass("error");
+            table_search_text_control_group.addClass("success");
         } else {
-            $("#table_search_text_control_group").addClass("error");
-            $("#table_search_text_control_group").removeClass("success");
+            table_search_text_control_group.addClass("error");
+            table_search_text_control_group.removeClass("success");
         }        
     }
 
-    $("#table_search_text").keyup(function(e) {
-        if ($(this).val() == previous_search_text) {
-            return false;
-        }
-        rc = parse_search_text($(this).val())
-        update_table_search_text_warning(rc);
-        if (rc) {
-            rows_to_display = get_rows_to_display(parsed_query);
-            $("#table tbody tr").hide();
-            _.each(rows_to_display, function(row) { $(row).show(); });
-            console.log(rows_to_display);
-        } else {
-            $("#table tbody tr").show();
-        }
-        previous_search_text = $(this).val();
-    });
+    maybe_update_table_headings = function () {
+        if (_.isEmpty(table_headings)) {
+            $.map(table.find("th"), function(el, i) {
+                table_headings[el.textContent.toLowerCase()] = i; 
+            });
+            console.log(table_headings);
+        }            
+    }
 
-    $("#table_search_text").keypress(function(e) {
-       return (e.keyCode || e.which || e.charCode || 0) !== 13; 
-    });
+    set_table = function(selector) {
+        table = $(selector);
+    }
 
-    $("#table_search_error").hide();
+    set_table_search_text = function(selector) {
+        table_search_text = $(selector);
+        table_search_text_control_group = table_search_text.closest(".control-group");
+        table_search_text.keyup(function(e) {
+            maybe_update_table_headings();
+            if ($(this).val() == previous_search_text) {
+                return false;
+            }
+            var rv = parse_search_text($(this).val())
+            update_table_search_text_warning(rv.rc);
+            if (rv.rc) {
+                var rows_to_display = get_rows_to_display(rv.parsed_query);
+                table.find("tbody tr").hide();
+                _.each(rows_to_display, function(row) { $(row).show(); });
+                console.log(rows_to_display);
+            } else {
+                table.find("tbody tr").show();
+            }
+            previous_search_text = $(this).val();
+        });
+
+        table_search_text.keypress(function(e) {
+           return (e.keyCode || e.which || e.charCode || 0) !== 13; 
+        });
+    }
 
 });
