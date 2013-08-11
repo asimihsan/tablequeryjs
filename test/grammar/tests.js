b5a6008c88c5b8adb353b564d3669e25c52217b5
Asim Ihsan <asim.ihsan@gmail.com>
2013-08-11 19:02:04 +0100

    test/grammar - add inequality tests using '!=' and NOT
    Mainly trying to kick Travis CI to run a test.

--- test/grammar/tests.js
+++ test/grammar/tests.js
@@ -8,6 +8,17 @@ test( "equality with '=='", function() {
               ['EQ', 'index', '5']);
 });
 
+test( "inequality with '!='", function() {
+    deepEqual(grammar.parse("index != 5"),
+              ['NEQ', 'index', '5']);
+});
+
+test( "inequality with NOT and '='", function() {
+    deepEqual(grammar.parse("not index = 5"),
+              ['NOT',
+                ['EQ', 'index', '5']]);
+});
+
 test( "simple two statements with OR and NOT", function() {
     deepEqual(grammar.parse("index = 5 or not ('first name' = dude)"),
               ['OR',
