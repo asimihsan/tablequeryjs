15a24492b7876e0c172154f890b1d83dda67ed15
Asim Ihsan <angry.blade5619@ihsan.io>
2013-08-17 14:55:24 +0100

    build - use Grunt to build everything.
    - Dependencies are included inside a closure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,85 @@
 module.exports = function(grunt) {
     // Project config
     grunt.initConfig({
+        shell: {
+            multiple: {
+                command: [
+                    'jison src/grammar.jison --outfile build/grammar.js',
+                    'sed -i "" s/window.Modernizr/tablequery.Modernizr/g build/modernizr.custom.js',
+                    'sed -i "" s/Modernizr.load/tablequery.Modernizr.load/g build/modernizr.custom.js'
+                ].join('&&'),
+                options: {
+                    stdout: true
+                }
+            }
+        },
+        lodash: {
+            build: {
+                dest: 'build/lodash.custom.js'
+            },
+            options: {
+                modifier: 'modern',
+                include: [
+                    'intersection',
+                    'union',
+                    'filter',
+                    'map',
+                    'each',
+                    'memoize',
+                    'debounce',
+                    'contains'
+                ],
+                flags: [
+                    '--debug'
+                ]
+            }
+        },
+        bower: {
+            install: {
+            }
+        },
+        modernizr: {
+            devFile: "lib/modernizr/modernizr.js",
+            outputFile: "build/modernizr.custom.js",
+            uglify: false,
+            tests: [
+                "localstorage"
+            ],
+            files: [
+                'src/tablequery-core.js'
+            ]
+        },
+        uglify: {
+            options: {
+                banner: '/*! tablequeryjs by Asim Ihsan (http://www.asimihsan.com)\n https://github.com/asimihsan/tablequeryjs\n <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                wrap: "tablequery",
+                /*
+                mangle: false,
+                beautify: true
+                */
+            },
+            build: {
+                src: [
+                    'build/lodash.custom.js',
+                    'build/modernizr.custom.js',
+                    'build/grammar.js',
+                    'src/tablequery-core.js'
+                ],
+                dest: 'build/tablequery.min.js'
+            }
+        },
         qunit: {
             files: ['test/grammar/tests.html']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-qunit');
-
-    // Task to run tests
     grunt.registerTask('test', 'qunit');
+
+    grunt.loadNpmTasks('grunt-lodash');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-modernizr');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('build', ['bower', 'lodash', 'modernizr', 'shell', 'uglify']);
 }
