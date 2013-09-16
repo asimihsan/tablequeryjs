6d6a469380217ad7e770186c5190a63e2ebe9e58
Asim Ihsan <asim.ihsan@youview.com>
2013-09-16 15:08:02 +0100

    date - fix robustness to empty text values in table of data
    This commit fixes tablequeryjs to be robust to tables of data
    with blank values. This broke the datetime parsing and caused
    unexpected results when detecting numbers. This commit also
    adds some robustness tests to make sure this doesn't regress.

---
+++ test/tablequery-core-robustness/test_simple_eq.js
@@ -0,0 +1,28 @@
+var casper = require('casper').create({
+    clientScripts: ["lib/jquery/jquery.js"]
+});
+var x = require('casper').selectXPath;
+
+casper.start('./test/tablequery-core-robustness/index.html', function() {
+    casper.waitForSelector('#table_search_text', undefined, undefined, 2000);
+});
+
+casper.then(function() {
+    this.test.assertExists("#table_search_text");
+    this.evaluate(function() {
+        $("#table_search_text").val("number = 1")
+                               .click()
+                               .blur();
+    });
+    this.wait(1000, function() {
+        this.capture('test/tablequery-core-robustness/test_simple_eq.png', undefined);
+        this.test.assertVisible(x(".//tbody/tr[1]"), "first row visisble");
+        this.test.assertNotVisible(x(".//tbody/tr[2]"), "second row not visible");
+        this.test.assertNotVisible(x(".//tbody/tr[3]"), "third row not visible");
+    });
+});
+
+casper.run(function() {
+    this.test.renderResults(true);
+    this.test.done();
+});
